b38b48fb360511e68eec5d13df2902fa6c532e2e
SerJason <jason@serduke.com>
2016-11-22 15:16:55 -0800

    Add initial Dashboard

---
+++ py/fitness.py
@@ -0,0 +1,23 @@
+def saveWorkedOut(workedOut):
+	# TODO: Save this data.
+	return True
+
+def getWorkedOut():
+	# TODO: Get this data.
+	return False
+
+def saveTrackedCalories(trackedCalories):
+	# TODO: Save this data.
+	return True
+
+def getTrackedCalories():
+	# TODO: Get this data.
+	return False
+
+def saveGotSleep(gotSleep):
+	# TODO: Save this data.
+	return True
+
+def getGotSleep(gotSleep):
+	# TODO: Get this data.
+	return False
