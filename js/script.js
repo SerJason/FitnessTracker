520499a8840d6c8c578efdfcc48e4a8eeb70a11d
davids549 <davidserduke@gmail.com>
2016-11-22 16:00:23 -0800

    Added promise tracking on the ajax user request

--- js/script.js
+++ js/script.js
@@ -1,9 +1,9 @@
 var User = {
   load: function () {
-    $.get("../py/getUser.py", function (data) {
+    User.jqXHR = $.getJSON("../py/getUser.py", function (data) {
           User.name = data.name;
           User.username = data.username;
-        }, "json");
+        });
   }
 };
 
